eda34d652f30b6c5dad1a17eaed31bb36392eba6
bohdan zhylavskyi <zhylavskyibohdan@gmail.com>
2025-12-18 16:21:39 +0200

    implement task #1

--- sources/Task1/Task1/Program.cs
+++ sources/Task1/Task1/Program.cs
@@ -21,15 +21,13 @@ namespace Task1
 
             var iterate = 10000;
 
-            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
-
+            using var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
             byte[] hash = pbkdf2.GetBytes(20);
 
             byte[] hashBytes = new byte[36];
 
-            Array.Copy(salt, 0, hashBytes, 0, 16);
-
-            Array.Copy(hash, 0, hashBytes, 16, 20);
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+            Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
 
             var passwordHash = Convert.ToBase64String(hashBytes);
 
